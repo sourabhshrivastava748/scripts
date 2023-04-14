c5cf92e4a59d9299f55f7c3a4e0bd8519fd8eb2e
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-04-14 06:57:57 +0530

    Change script

--- getProximityAllocationCount.py
+++ getProximityAllocationCount.py
@@ -1,11 +1,18 @@
 import pymongo, sys
 from datetime import date
 
-mongodbUri = sys.argv[1]
-myclient = pymongo.MongoClient(mongodbUri)
+# serverName = sys.argv[1]
+# commonPrimary = getClient()
+# mongodbUri = getTenantSpecificMongo(commonPrimary)
 
-dbsList = myclient.list_database_names()
-print("Database list: " + str(dbsList))
+myclient = getClient()
+# dbsList = myclient.list_database_names()
+dbsList = ['tcns', 'curefit', 'pep', 'mamaearth', 'leayanglobal', 'bestseller']
+print("Database(tenant) list: " + str(dbsList))
+
+fileName = "/tmp/proximity-based-allocation-" + date.today().strftime("%d-%m-%Y") + ".csv"
+f = open(fileName, "w")
+f.write("Tenant,Allocation Count,Date\n")
 
 colName = "methodActivityMeta"
 
@@ -21,10 +28,33 @@ for tenantCode in dbsList:
 		}
 
 		allocationCount = mycol.count_documents(query)
-		print(str(tenantCode) + "," + str(date.today()) + "," + str(allocationCount))
+		print(str(tenantCode) + "," + str(allocationCount) + "," + str(date.today().strftime("%d-%m-%Y")))
+		f.write(str(tenantCode) + "," + str(allocationCount) + "," + str(date.today().strftime("%d-%m-%Y")) + "\n")
+
+f.close()
+
+def getClient():
+	try:
+	    c = MongoClient('mongo1.e1-in.unicommerce.infra', 27017);
+	    db= c['uniwareConfig'];
+	    db.test.insert_one({});
+	except:
+	    print("Server not available common1.mongo.unicommerce.infra");
+	    common = 0;
+
+	if common == 0:
+	    print("common changed")
+	    c = MongoClient('mongo2.e1-in.unicommerce.infra', 27017);
+    return c
 
 
+# def getTenantSpecificMongo(commonPrimary):
+# 	db = commonPrimary['uniwareConfig'];
+# 	mycol = db["serverDetails"]
+# 	print ("Getting details for " + sys.argv[1]);
 
+# 	serverDetail = mycol.find_one({"name": sys.argv[1]});
+# 	print ("Tenant specific mongo hosts " + serverDetail["tenantSpecificMongoHosts"][0]);
 
 
 
