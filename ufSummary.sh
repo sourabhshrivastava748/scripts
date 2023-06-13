ef6a709e2aafffa9c539e387c993c0f38a8ea646
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-06-13 12:36:42 +0530

    Add oncall

--- ufSummary.sh
+++ ufSummary.sh
@@ -8,7 +8,7 @@ echo "Report file: ${reportFilename}"
 
 yesterday_date=$(date -d "yesterday 13:00" +'%d-%m-%Y')
 
-MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,kapil@unicommerce.com,prateek.mahajan@unicommerce.com,adarsh.bajpai@unicommerce.com,rakshit.jain@unicommerce.com"
+MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,dixit.garg@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,kapil@unicommerce.com,prateek.mahajan@unicommerce.com,adarsh.bajpai@unicommerce.com,rakshit.jain@unicommerce.com,oncall@unicommerce.com"
 MAIL_SUBJECT="Unfulfillable Sale Order Summary | ${yesterday_date}"
 MAIL_CONTENT="Please find the attachment. Report prepared by alpha team"
 
