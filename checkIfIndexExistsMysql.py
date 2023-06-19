301af9ecbc1c2c171e448e46d45f2a6f6b0617a1
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-06-19 17:41:53 +0530

    Refactor code

--- checkIfIndexExistsMysql.py
+++ checkIfIndexExistsMysql.py
@@ -7,7 +7,7 @@ dataBase = mysql.connector.connect(
   database = "uniware"
 )
 
-tableName = "sale_order"
+tableName = "shipping_package"
 indexColumns = ['tenant_id', 'channel_id', 'created']
 query = "show index from " + str(tableName)
 indexExists = False
@@ -19,23 +19,51 @@ indexList = cursorObject.fetchall()
 indexDict = dict()
 
    
-for index in indexList:
-    # print("Key_name: " + str(index[2]) + " Column_name : " + str(index[4]))
-    if index[2] not in indexDict.keys():
-        indexDict[index[2]] = set()
+# for index in indexList:
+#     # print("Key_name: " + str(index[2]) + " Column_name : " + str(index[4]))
+#     if index[2] not in indexDict.keys():
+#         indexDict[index[2]] = set()
 
-    indexDict[index[2]].add(index[4])
+#     indexDict[index[2]].add(index[4])
 
 
-for key in indexDict:
-    print(str(key) + " : " + str(indexDict[key]))
-    if (set(indexColumns) == set(indexDict[key])):
-        indexExists = True
-        print("Index exists for " + str(indexColumns) + " in table " + str(tableName) + " with name " + str(key))
-        break
+# for key in indexDict:
+#     print(str(key) + " : " + str(indexDict[key]))
+#     if (set(indexColumns) == set(indexDict[key])):
+#         indexExists = True
+#         print("Index exists for " + str(indexColumns) + " in table " + str(tableName) + " with name " + str(key))
+#         break
 
-if (not indexExists):
-    print("Index does not exists for " + str(indexColumns) + " in table " + str(tableName))
+# if (not indexExists):
+#     print("Index does not exists for " + str(indexColumns) + " in table " + str(tableName))
+
+# -------------- test --------------
+
+tableNameList = ["shipping_package", "shipping_manifest_item", "shipping_manifest"]
+
+for tableName in tableNameList:
+    print("------ " + str(tableName) + "--------")
+    query = "show index from " + str(tableName)
+
+    cursorObject = dataBase.cursor()
+    cursorObject.execute(query)
+       
+    indexList = cursorObject.fetchall()
+    indexDict = dict()
+
+    for index in indexList:
+        # print("Key_name: " + str(index[2]) + " Column_name : " + str(index[4]))
+        if index[2] not in indexDict.keys():
+            indexDict[index[2]] = set()
+
+        indexDict[index[2]].add(index[4])
+
+    for key in indexDict:
+        print(str(key) + " : " + str(indexDict[key]))
+
+    print("")
+
+# -------------- --------------
 
   
 # Disconnecting from the server
