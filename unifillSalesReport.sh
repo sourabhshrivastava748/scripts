53b6f96d327ed2584f2b0ee3fb84177dc48529a6
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-08-23 19:14:58 +0530

    Refactor code

--- unifillSalesReport.sh
+++ unifillSalesReport.sh
@@ -6,7 +6,7 @@ echo "Report file: ${reportFilename}"
 
 today_date=$(date +'%d-%b-%Y')
 
-MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
+MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com"
 
 MAIL_SUBJECT="Unifill Sales Report MTD | ${today_date}"
 MAIL_CONTENT="Please find the attachment"
