b978dc76eb54cb7dd853aad799e2892f80935a86
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-08-23 16:25:34 +0530

    Refactor code

--- unifillSalesReport.py
+++ unifillSalesReport.py
@@ -4,34 +4,35 @@ from pymongo import MongoClient
 from collections import Counter
 import mysql.connector
 
+
+# Create report file
+fromDate = datetime.date.today() - datetime.timedelta(days = 60)
+fromDateString = fromDate.strftime("%Y-%m-%d")
+toDateString = datetime.date.today().strftime("%Y-%m-%d")
+
+print("fromDate: " + fromDateString)
+print("toDate: " + toDateString)
+
+outputFileName = "/tmp/unifill-sales-report_" + fromDateString  + "_to_" + toDateString  + ".csv"
+outputFile = open(outputFileName, "w")
+outputFile.write("Tenant,LookupsFound,FromDate,ToDate\n")
+
+
+mysqlDbUri = "db.address.unicommerce.infra"
+dbName = "turbo"
+
+mysqlDbClient = mysql.connector.connect(
+  host = mysqlDbReplicaUri,
+  user ="developer",
+  passwd ="DevelopeR@4#",
+  database = dbName
+)
+mysqlDbCursor = mysqlDbClient.cursor();
+
+tenantLookupQuery = "select tenant_code, count(lookup_status) from address_lookup_trace where tenant_code in (select distinct(tenant_code) from tenant_details) and lookup_status = 'FOUND' and created_at between " + fromDateString + " and " + toDateString + " group by tenant_code;"
+print("tenantLookupQuery : " + tenantLookupQuery)
+
 try:
-	# Create report file
-	fromDate = datetime.date.today() - datetime.timedelta(days = 60)
-	fromDateString = fromDate.strftime("%Y-%m-%d")
-	toDateString = datetime.date.today().strftime("%Y-%m-%d")
-
-	print("fromDate: " + fromDateString)
-	print("toDate: " + toDateString)
-
-	outputFileName = "/tmp/unifill-sales-report_" + fromDateString  + "_to_" + toDateString  + ".csv"
-	outputFile = open(outputFileName, "w")
-	outputFile.write("Tenant,LookupsFound,FromDate,ToDate\n")
-
-
-	mysqlDbUri = "db.address.unicommerce.infra"
-	dbName = "turbo"
-
-	mysqlDbClient = mysql.connector.connect(
-	  host = mysqlDbReplicaUri,
-	  user ="developer",
-	  passwd ="DevelopeR@4#",
-	  database = dbName
-	)
-	mysqlDbCursor = mysqlDbClient.cursor();
-
-	tenantLookupQuery = "select tenant_code, count(lookup_status) from address_lookup_trace where tenant_code in (select distinct(tenant_code) from tenant_details) and lookup_status = 'FOUND' and created_at between " + fromDateString + " and " + toDateString + " group by tenant_code;"
-	print("tenantLookupQuery : " + tenantLookupQuery)
-
 	mysqlDbCursor.execute(soiCountQuery)
 
 	print("Tenant,LookupsFound,FromDate,ToDate")
