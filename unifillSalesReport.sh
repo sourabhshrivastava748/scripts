0fb13d6f486a81195265c1054776f07058bedd0f
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-08-26 09:04:31 +0530

    Refactor code

--- unifillSalesReport.sh
+++ unifillSalesReport.sh
@@ -1,7 +1,7 @@
 python3 unifillSalesReport.py
 
-ls -1 /tmp/unifill-mtd-sales-report* 
-reportFilename=`ls -1t /tmp/unifill-mtd-sales-report* | head -1`
+ls -1 /tmp/unifill-sales-report* 
+reportFilename=`ls -1t /tmp/unifill-sales-report* | head -1`
 echo "Report file: ${reportFilename}"
 
 yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
@@ -9,7 +9,7 @@ yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
 # MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,anurag.mittal@unicommerce.com,accounts@unicommerce.com"
 MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
 
-MAIL_SUBJECT="Unifill Sales Report MTD | ${yesterday_date}"
+MAIL_SUBJECT="Unifill Sales Report | ${yesterday_date}"
 MAIL_CONTENT="Please find the attachment. Report prepared by alpha team."
 
 echo ${MAIL_CONTENT} | mutt -s "${MAIL_SUBJECT}" -a "${reportFilename}" -- "${MAIL_RECIPIENTS}"
