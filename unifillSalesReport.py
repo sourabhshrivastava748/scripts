b99a2b2d61afe01503609ce26f2d4112ea9fb796
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-08-27 08:38:52 +0530

    Refactor code

--- unifillSalesReport.py
+++ unifillSalesReport.py
@@ -7,22 +7,21 @@ import mysql.connector
 
 
 # FromDate : First day of the month.. ToDate : Yesterday
+fromDate = datetime.date.today().replace(day=1)
+fromDateString = fromDate.strftime("%Y-%m-%d")
+toDate = datetime.date.today() - datetime.timedelta(days = 1)
+toDateString = toDate.strftime("%Y-%m-%d")
 
-# fromDate = datetime.date.today().replace(day=1)
-# fromDateString = fromDate.strftime("%Y-%m-%d")
-# toDate = datetime.date.today() - datetime.timedelta(days = 1)
-# toDateString = toDate.strftime("%Y-%m-%d")
-
-fromDateString = "2023-07-18"
-toDateString = "2023-08-17"
+# fromDateString = "2023-07-18"
+# toDateString = "2023-08-17"
 
 print("-- Unifill Sales Report MTD --")
 print("fromDate: " + fromDateString)
 print("toDate: " + toDateString)
 
 # Create report file
-# outputFileName = "/tmp/unifill-mtd-sales-report_" + toDateString  + ".csv"
-outputFileName = "/tmp/unifill-sales-report_" + fromDateString + "_to_" + toDateString  + ".csv"
+outputFileName = "/tmp/unifill-mtd-sales-report_" + toDateString  + ".csv"
+# outputFileName = "/tmp/unifill-sales-report_" + fromDateString + "_to_" + toDateString  + ".csv"
 
 outputFile = open(outputFileName, "w")
 
@@ -38,18 +37,20 @@ mysqlDbClient = mysql.connector.connect(
 )
 mysqlDbCursor = mysqlDbClient.cursor();
 
-# unifillReportQuery = "SELECT tenant_code, count(*) AS total_lookups, SUM(CASE WHEN lookup_status = 'FOUND' THEN 1 ELSE 0 END) AS lookups_found, SUM(CASE WHEN lookup_status = 'NOT_FOUND' THEN 1 ELSE 0 END) AS lookups_not_found, COUNT(DISTINCT CASE WHEN lookup_status = 'FOUND' THEN mobile END) AS lookups_found_unique_mobiles FROM address_lookup_trace WHERE     tenant_code IN (SELECT DISTINCT(tenant_code) FROM tenant_details) AND created_at BETWEEN '" + fromDateString + "' AND '" + toDateString + "' GROUP BY tenant_code;"
+unifillReportQuery = "SELECT tenant_code, count(*) AS total_lookups, SUM(CASE WHEN lookup_status = 'FOUND' THEN 1 ELSE 0 END) AS lookups_found, SUM(CASE WHEN lookup_status = 'NOT_FOUND' THEN 1 ELSE 0 END) AS lookups_not_found, COUNT(DISTINCT CASE WHEN lookup_status = 'FOUND' THEN mobile END) AS lookups_found_unique_mobiles FROM address_lookup_trace WHERE     tenant_code IN (SELECT DISTINCT(tenant_code) FROM tenant_details) AND created_at BETWEEN '" + fromDateString + "' AND '" + toDateString + "' GROUP BY tenant_code;"
 
 
-unifillReportQuery = "SELECT tenant_code, count(*) AS total_lookups, SUM(CASE WHEN lookup_status = 'FOUND' THEN 1 ELSE 0 END) AS lookups_found, SUM(CASE WHEN lookup_status = 'NOT_FOUND' THEN 1 ELSE 0 END) AS lookups_not_found, COUNT(DISTINCT CASE WHEN lookup_status = 'FOUND' THEN mobile END) AS lookups_found_unique_mobiles FROM address_lookup_trace WHERE     tenant_code IN (SELECT DISTINCT(tenant_code) FROM tenant_details) AND created_at BETWEEN '" + fromDateString + "' AND '" + toDateString + "' AND mobile NOT IN ('+919999900000','+919999911111','+919999999999','+919901033800','+919711903969','+919428140479','+919015238030','+919315974807','+919663265675','+919468283901','+918126148700','+918700789547') GROUP BY tenant_code;"
+# unifillReportQuery = "SELECT tenant_code, count(*) AS total_lookups, SUM(CASE WHEN lookup_status = 'FOUND' THEN 1 ELSE 0 END) AS lookups_found, SUM(CASE WHEN lookup_status = 'NOT_FOUND' THEN 1 ELSE 0 END) AS lookups_not_found, COUNT(DISTINCT CASE WHEN lookup_status = 'FOUND' THEN mobile END) AS lookups_found_unique_mobiles FROM address_lookup_trace WHERE     tenant_code IN (SELECT DISTINCT(tenant_code) FROM tenant_details) AND created_at BETWEEN '" + fromDateString + "' AND '" + toDateString + "' AND mobile NOT IN ('+919999900000','+919999911111','+919999999999','+919901033800','+919711903969','+919428140479','+919015238030','+919315974807','+919663265675','+919468283901','+918126148700','+918700789547') GROUP BY tenant_code;"
 
 print("unifillReportQuery : " + unifillReportQuery)
 
 try:
 	mysqlDbCursor.execute(unifillReportQuery)
 
-	outputFile.write("Tenant,TotalLookups,LookupsFound,LookupsNotFound,UniqueMobileForLookupsFound,Date\n")
-	print("Tenant,TotalLookups,LookupsFound,LookupsNotFound,UniqueMobileForLookupsFound,Date")
+	columnHeadings = "Tenant,TotalLookups,LookupsFound,LookupsNotFound,UniqueMobileForLookupsFound,Date"
+	print(columnHeadings)
+
+	outputFile.write(columnHeadings + "\n")
 	
 	for row in mysqlDbCursor.fetchall():
 		tenant = row[0]
@@ -57,7 +58,12 @@ try:
 		lookupsFound = row[2]
 		lookupsNotFound = row[3]
 		uniqueMobileForLookupsFound = row[4]
-		summary = str(tenant) + "," + str(totalLookups) + "," + str(lookupsFound) + "," + str(lookupsNotFound) + "," + str(uniqueMobileForLookupsFound) + "," + toDateString
+		summary = str(tenant) + "," 
+						+ str(totalLookups) + "," 
+						+ str(lookupsFound) + "," 
+						+ str(lookupsNotFound) + "," 
+						+ str(uniqueMobileForLookupsFound) + "," 
+						+ toDateString
 		print(summary)
 		outputFile.write(summary + "\n")
 
