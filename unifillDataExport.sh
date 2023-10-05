d7dd8fd924ff4cc7cfbc1bedbabbeb1c37ddef1a
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-10-05 14:33:07 +0530

    Refactor code

--- unifillDataExport.sh
+++ unifillDataExport.sh
@@ -16,10 +16,11 @@ else
     current_date=$(date +'%d-%b-%Y %H:%M')
 
 	MAIL_RECIPIENTS=${EmailRecipientList}
-	MAIL_RECIPIENTS_CC="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com "
+	# MAIL_RECIPIENTS_CC="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com"
+	MAIL_RECIPIENTS_CC="sourabh.shrivastava@unicommerce.com"
 
 	MAIL_SUBJECT="Unifill Data Export | ${current_date}"
-	MAIL_CONTENT="Please find the attachment."
+	MAIL_CONTENT="Please find the attachment. Build triggered by ${BUILD_USER}"
 
 	echo ${MAIL_CONTENT} | mutt -s "${MAIL_SUBJECT}" -a "${reportFilename}" -c "${MAIL_RECIPIENTS_CC}" -- "${MAIL_RECIPIENTS}"
 fi
