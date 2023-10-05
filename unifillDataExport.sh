3d0e5deaa17796e51f885333eda84bb30abe5463
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-10-05 12:03:43 +0530

    Refactor code

---
+++ unifillDataExport.sh
@@ -0,0 +1,26 @@
+# python3 unifillSalesReport.py
+echo "Unifill data export"
+
+echo $MobileList
+
+
+# Send mail
+ls -1 /tmp/unifill-data-export* 
+reportFilename=`ls -1t /tmp/unifill-data-export* | head -1`
+echo "Report file: ${reportFilename}"
+
+if [ -z "$reportFilename" ];
+then
+    echo "Report file is empty."
+else
+    current_date=$(date +'%d-%b-%Y %H:%M')
+
+	MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
+
+	MAIL_SUBJECT="Unifill Data Export | ${current_date}"
+	MAIL_CONTENT="Please find the attachment."
+
+	echo ${MAIL_CONTENT} | mutt -s "${MAIL_SUBJECT}" -a "${reportFilename}" -- "${MAIL_RECIPIENTS}"
+fi
+
+
