955af6106f7c4272dd16cf2ebe24f375723385d0
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-10-25 15:06:46 +0530

    inventory sync timeline script

---
+++ UniwareDbUtility.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+import sys, datetime, pytz, re
+from pymongo import MongoClient
+from collections import Counter
+import mysql.connector
+
+def getClient(uri1, uri2):
+	try:
+	    c = MongoClient(uri1, 27017);
+	    db= c['uniwareConfig'];
+	    db.test.insert_one({});
+	    print(str(uri1))
+	except:
+	    c = MongoClient(uri2, 27017);
+	    print(str(uri2) + " - common changed")
+
+	return c
+
+
+def getServerNameFromTenant(tenantCode):
+	commonMongoUri1 = "common1.mongo.unicommerce.infra:27017"
+	commonMongoUri2 = "common2.mongo.unicommerce.infra:27017"
+	commonMongoClient = getClient(commonMongoUri1, commonMongoUri2)
+	db = commonMongoClient['uniware']
+	col = db['tenantProfile']
+
+	return col.find_one({"tenantCode" : tenantCode})['serverName']
+
+
+def getTenantSpecificMongoFromServerName(serverName):
+	commonMongoUri1 = "common1.mongo.unicommerce.infra:27017"
+	commonMongoUri2 = "common2.mongo.unicommerce.infra:27017"
+	commonMongoClient = getClient(commonMongoUri1, commonMongoUri2)
+	db = commonMongoClient['uniwareConfig']
+	col = db['serverDetails']
+
+	return col.find_one({"name" : serverName})['tenantSpecificMongoHosts']
+
+
+def getTenantSpecificMongoUri(tenantCode):
+	serverName = getServerNameFromTenant(tenantCode)
+	print(tenantCode + ", Server Name: " + serverName)
+	mongoUri = []
+	if (serverName):
+		mongoUri = getTenantSpecificMongoFromServerName(serverName)
+		print("Server Name: " + serverName + ", mongoUri: " + str(mongoUri))
+	else :
+		print("Cannot find serverName for " + str(tenantCode))
+
+	return mongoUri
+
+
+def getMysqlDBFromServerName(serverName):
+	# Get Mysql DB replica 
+	commonMongoUri1 = "common1.mongo.unicommerce.infra:27017"
+	commonMongoUri2 = "common2.mongo.unicommerce.infra:27017"
+	commonMongoClient = getClient(commonMongoUri1, commonMongoUri2)
+	db = commonMongoClient['uniwareConfig']
+	col = db['serverDetails']
+
+	return col.find_one({"name" : serverName})['replicatedTo']
+
+
+
+def getDbNameFromServerName(serverName):
+	dedicatedReplica = ["Myntra", "Lenskart", "BrandStudio", "MamaEarth", "Ril", "ECloud3" , "ECloud2" , "ECloud1", "ECloud4", "Dhani", "ECloud5", "ECloud6", "Cloud37", "UniStage"]
+
+	if (serverName in dedicatedReplica):
+		return "uniware"
+	else:
+		return serverName
+
---
+++ inventorySyncTimeline.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+import sys, datetime, pytz, re
+import UniwareDbUtility 
+
+
+def getSummary(inventorySyncData, tenantCode, date):
+	summary = ""
+	for data in inventorySyncData:
+		summary += (
+			tenantCode + ","
+			data["requestIdentifier"] + "," 
+			data["totalMarkDirtyTimeInSeconds"] + "," 
+			data["totalChannelSyncTimeInSeconds"] + "," 
+			data["totalTimeInSeconds"] + "," 
+			data["totalCit"] + "," 
+			data["markDirtyTimePerCit"] + "," 
+			data["channelSyncTimePerCit"] + "," 
+			date)
+
+	return summary
+
+
+# 										-- Main --
+try:
+	colName = "channelInventoryUpdate_O2"
+
+	midnightDateTime_today = datetime.datetime.today().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+	midnightDateTime_yesterday = midnightDateTime_today - datetime.timedelta(days = 1)
+
+	utcMidnightDateTime_today = midnightDateTime_today.astimezone(pytz.UTC)
+	utcMidnightDateTime_yesterday = midnightDateTime_yesterday.astimezone(pytz.UTC)
+
+	reportDate = datetime.date.today() - datetime.timedelta(days = 1)
+	reportDateStr = reportDate.strftime("%d-%m-%Y")
+
+	print("utcMidnightDateTime_today: " + str(utcMidnightDateTime_today))
+	print("utcMidnightDateTime_yesterday: " + str(utcMidnightDateTime_yesterday))
+	print("reportDate: " + str(reportDate))
+
+	# Create output file
+	outputFileName = "/tmp/inventory-sync-timeline-" + reportDateStr + ".csv"
+	outputFile = open(outputFileName, "w")
+	outputFile.write("Tenant,RequestIdentifier,TotalMarkDirtyTimeInSeconds,TotalChannelSyncTimeInSeconds,TotalTimeInSeconds,TotalCit,MarkDirtyTimePerCit,ChannelSyncTimePerCit,Date\n")
+
+	tenantList = ["helioslifestyle"]
+
+	# For all tenants
+	for tenant in tenantList:
+		try:
+			# Get mongodbUri of tenant 			
+			mongoUri = []
+			mongoUri = UniwareDbUtility.getTenantSpecificMongoUri(tenant)
+
+			if (len(mongoUri) == 2):
+				# Create mongo client
+				myclient = UniwareDbUtility.getClient(mongoUri[0], mongoUri[1])
+				mydb = myclient[tenant]
+				mycol = mydb[colName]
+
+				# Get inventory sync data
+				aggregationSteps = [
+				  {
+				    "$match": {
+				      "created": {
+				        "$gte": utcMidnightDateTime_yesterday,
+				        "$lte": utcMidnightDateTime_today
+				      },
+				      "itisUnacknowledgedTime": { "$exists": "true" },
+				      "calculatedInventory": {
+				        "$lt": "5"
+				      }
+				    }
+				  },
+				  {
+				    "$group": {
+				      _id: "$requestIdentifier",
+				      totalMarkDirtyTimeInSeconds: {
+				        "$sum": {
+				          "$divide": [
+				            { "$subtract": ["$citMarkDirtyTime", "$itisUnacknowledgedTime"] },
+				            1000
+				          ]
+				        }
+				      },
+				      "totalChannelSyncTimeInSeconds": {
+				        "$sum": {
+				          "$divide": [
+				            { "$subtract": ["$created", "$citMarkDirtyTime"] },
+				            1000
+				          ]
+				        }
+				      },
+				      "totalTimeInSeconds": {
+				        "$sum": {
+				          "$divide": [
+				            { "$subtract": ["$created", "$itisUnacknowledgedTime"] },
+				            1000
+				          ]
+				        }
+				      },
+				      "totalCit": { "$sum": 1 }
+				    }
+				  },
+				  {
+				    "$project": {
+				      "requestIdentifier": "$_id",
+				      "totalMarkDirtyTimeInSeconds": "$totalMarkDirtyTimeInSeconds",
+				      "totalChannelSyncTimeInSeconds": "$totalChannelSyncTimeInSeconds",
+				      "totalTimeInSeconds": "$totalTimeInSeconds",
+				      "totalCit": "$totalCit",
+				      "markDirtyTimePerCit": {
+				        "$divide": ["$totalMarkDirtyTimeInSeconds", "$totalCit"]
+				      },
+				      "channelSyncTimePerCit": {
+				        "$divide": ["$totalChannelSyncTimeInSeconds", "$totalCit"]
+				      }
+				    }
+				  }
+				]
+
+				inventorySyncData = list(mycol.aggregate(aggregationSteps)) 			
+
+				# Get Summary
+				summary = getSummary(inventorySyncData, tenant, str(reportDateStr))
+				print(summary)
+				outputFile.write(summary + "\n")
+
+		except Exception as e:
+			print("Exception while calculating uf data for tenant: " + tenant['code'])
+			print(e)
+
+except Exception as e:
+	print(e)
+	print(sys.exc_info()[0]);
+	print("FAILED");
+
+finally:
+	outputFile.close()
+
+
