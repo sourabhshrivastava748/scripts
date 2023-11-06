53fd9cee356fbdfb90e1cc0bf1e63a03d24ec23a
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-11-06 08:34:34 +0530

    Add parameterized script

---
+++ unifillSalesReport-Parameterized.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import sys, datetime, pytz, re
+from pymongo import MongoClient
+from collections import Counter
+import mysql.connector
+
+
+fromDateString = sys.argv[1]
+toDateString = sys.argv[2]
+
+# fromDateString = "2023-07-18"
+# toDateString = "2023-08-17"
+
+print("-- Unifill Sales Report Parameterized --")
+print("fromDate: " + fromDateString)
+print("toDate: " + toDateString)
+
+# Create report file
+outputFileName = "/tmp/unifill-sales-report_" + fromDateString + "_to_" + toDateString  + ".csv"
+outputFile = open(outputFileName, "w")
+
+mysqlDbUri = "db.address.unicommerce.infra"
+dbName = "turbo"
+
+mysqlDbClient = mysql.connector.connect(
+  host = mysqlDbUri,
+  user ="developer",
+  passwd ="DevelopeR@4#",
+  database = dbName
+)
+mysqlDbCursor = mysqlDbClient.cursor();
+
+unifillReportQuery = "SELECT tenant_code, count(*) AS total_lookups, SUM(CASE WHEN lookup_status = 'FOUND' THEN 1 ELSE 0 END) AS lookups_found, SUM(CASE WHEN lookup_status = 'NOT_FOUND' THEN 1 ELSE 0 END) AS lookups_not_found, COUNT(DISTINCT CASE WHEN lookup_status = 'FOUND' THEN mobile END) AS lookups_found_unique_mobiles FROM address_lookup_trace WHERE     tenant_code IN (SELECT DISTINCT(tenant_code) FROM tenant_details) AND created_at >= '" + fromDateString + "' AND created_at < '" + todayDateString + "' GROUP BY tenant_code;"
+
+
+# unifillReportQuery = "SELECT tenant_code, count(*) AS total_lookups, SUM(CASE WHEN lookup_status = 'FOUND' THEN 1 ELSE 0 END) AS lookups_found, SUM(CASE WHEN lookup_status = 'NOT_FOUND' THEN 1 ELSE 0 END) AS lookups_not_found, COUNT(DISTINCT CASE WHEN lookup_status = 'FOUND' THEN mobile END) AS lookups_found_unique_mobiles FROM address_lookup_trace WHERE     tenant_code IN (SELECT DISTINCT(tenant_code) FROM tenant_details) AND created_at BETWEEN '" + fromDateString + "' AND '" + toDateString + "' AND mobile NOT IN ('+919999900000','+919999911111','+919999999999','+919901033800','+919711903969','+919428140479','+919015238030','+919315974807','+919663265675','+919468283901','+918126148700','+918700789547') GROUP BY tenant_code;"
+
+print("unifillReportQuery : " + unifillReportQuery)
+
+try:
+	mysqlDbCursor.execute(unifillReportQuery)
+
+	columnHeadings = "Tenant,TotalLookups,LookupsFound,LookupsNotFound,UniqueMobileForLookupsFound,Date"
+	print(columnHeadings)
+
+	outputFile.write(columnHeadings + "\n")
+	
+	for row in mysqlDbCursor.fetchall():
+		tenant = row[0]
+		totalLookups = row[1]
+		lookupsFound = row[2]
+		lookupsNotFound = row[3]
+		uniqueMobileForLookupsFound = row[4]
+		summary = (str(tenant) + "," 
+						+ str(totalLookups) + "," 
+						+ str(lookupsFound) + "," 
+						+ str(lookupsNotFound) + "," 
+						+ str(uniqueMobileForLookupsFound) + "," 
+						+ toDateString)
+		print(summary)
+		outputFile.write(summary + "\n")
+
+	print("-- FINISHED --")	
+
+except Exception as e:
+	print(e)
+	print(sys.exc_info()[0]);
+	print("FAILED");
+
+finally:
+	mysqlDbClient.close()
+	outputFile.close()
+
--- unifillSalesReport.sh
+++ unifillSalesReport.sh
@@ -1,22 +1,48 @@
+#!/bin/bash
 
-python3 unifillSalesReport.py
-python3 unifillSalesReport-DailyUsage.py
+if [ "$#" -eq 0 ]; then
+    echo "Cron run"
+    python3 unifillSalesReport.py
+	python3 unifillSalesReport-DailyUsage.py
 
-ls -1 /tmp/unifill-mtd-sales-report* 
-reportFilename=`ls -1t /tmp/unifill-mtd-sales-report* | head -1`
-echo "Report file: ${reportFilename}"
+	ls -1 /tmp/unifill-mtd-sales-report* 
+	reportFilename=`ls -1t /tmp/unifill-mtd-sales-report* | head -1`
+	echo "Report file: ${reportFilename}"
 
-ls -1 /tmp/unifill-sales-report-daily-usage* 
-reportFilename2=`ls -1t /tmp/unifill-sales-report-daily-usage* | head -1`
-echo "Report file 2: ${reportFilename2}"
+	ls -1 /tmp/unifill-sales-report-daily-usage* 
+	reportFilename2=`ls -1t /tmp/unifill-sales-report-daily-usage* | head -1`
+	echo "Report file 2: ${reportFilename2}"
 
-yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
+	yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
 
-MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,anurag.mittal@unicommerce.com,accounts@unicommerce.com,financeteam@unicommerce.com,abhinav.gupta@unicommerce.com,kapil@unicommerce.com,lalit.sharma@unicommerce.com,pramod@unicommerce.com"
+	# MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,anurag.mittal@unicommerce.com,accounts@unicommerce.com,financeteam@unicommerce.com,abhinav.gupta@unicommerce.com,kapil@unicommerce.com,lalit.sharma@unicommerce.com,pramod@unicommerce.com"
 
-# MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
+	MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
 
-MAIL_SUBJECT="Unifill Sales Report MTD and Daily Usage | ${yesterday_date}"
-MAIL_CONTENT="Please find the attachment. Report prepared by alpha team."
+	MAIL_SUBJECT="Unifill Sales Report MTD and Daily Usage | ${yesterday_date}"
+	MAIL_CONTENT="Please find the attachment. Report prepared by alpha team."
 
-echo ${MAIL_CONTENT} | mutt -s "${MAIL_SUBJECT}" -a "${reportFilename}" -a "${reportFilename2}" -- "${MAIL_RECIPIENTS}"
+	echo ${MAIL_CONTENT} | mutt -s "${MAIL_SUBJECT}" -a "${reportFilename}" -a "${reportFilename2}" -- "${MAIL_RECIPIENTS}"
+
+elif [ "$#" -eq 3 ]; then
+    echo "Manual parameterized run with arguments: $#"
+    python3 unifillSalesReport-Parameterized.py "$1" "$2" 
+
+    reportFileString="unifill-sales-report_${1}_to_${2}.csv"
+    ls -1 ${reportFileString}
+    reportFilename=`ls -1t ${reportFileString} | head -1`
+    echo "Report file: ${reportFilename}"
+
+    MAIL_RECIPIENTS="$3"
+    echo "MAIL_RECIPIENTS: ${MAIL_RECIPIENTS}"
+    MAIL_SUBJECT="Unifill Sales Report | ${1} to ${2}"
+	MAIL_CONTENT="Please find the attachment. Report prepared by alpha team."
+
+	echo ${MAIL_CONTENT} | mutt -s "${MAIL_SUBJECT}" -a "${reportFilename}" -- "${MAIL_RECIPIENTS}"
+else
+    echo "Illegal number of arguments: $#"
+fi 
+
+
+
+
