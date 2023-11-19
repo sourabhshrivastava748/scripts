64f396d265a0c08890d0c0c798813445f0dd685d
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-11-19 19:17:53 +0530

    Change mail recipient to turbo

--- unifillSalesReport.sh
+++ unifillSalesReport.sh
@@ -15,7 +15,7 @@ if [ "$#" -eq 0 ]; then
 
 	yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
 
-	MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,anurag.mittal@unicommerce.com,accounts@unicommerce.com,financeteam@unicommerce.com,abhinav.gupta@unicommerce.com,kapil@unicommerce.com,lalit.sharma@unicommerce.com,pramod@unicommerce.com"
+	MAIL_RECIPIENTS="turbo@unicommerce.com"
 
 	# MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
 
