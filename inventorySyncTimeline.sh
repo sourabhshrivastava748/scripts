c24ddb6bdb3e41042a35c394ecfeddab6457109c
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2024-02-01 09:54:55 +0530

    Refactor code

--- inventorySyncTimeline.sh
+++ inventorySyncTimeline.sh
@@ -8,7 +8,7 @@ yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
 
 # MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,dixit.garg@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,kapil@unicommerce.com,prateek.mahajan@unicommerce.com,adarsh.bajpai@unicommerce.com,rakshit.jain@unicommerce.com,oncall@unicommerce.com"
 
-MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,ankur.pratik@unicommerce.com"
+MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
 
 MAIL_SUBJECT="Inventory sync timeline summary | ${yesterday_date}"
 MAIL_CONTENT="Please find the attachment. Report prepared by alpha team"
