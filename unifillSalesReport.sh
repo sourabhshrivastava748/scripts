1538b0efea96897307c8b62909a99df09887ebea
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2024-05-15 17:39:45 +0530

    Add html report

--- unifillSalesReport.sh
+++ unifillSalesReport.sh
@@ -20,12 +20,12 @@ if [ "$#" -eq 0 ]; then
 
 	ls -1 /tmp/unifill-count-api-mtd* 
 	reportFilename3=`ls -1t /tmp/unifill-count-api-mtd* | head -1`
-	echo "Report file: ${reportFilename}"
+	echo "Report file: ${reportFilename3}"
 	cp ${reportFilename3} ./
 
 	ls -1 /tmp/unifill-count-api-daily-usage* 
 	reportFilename4=`ls -1t /tmp/unifill-count-api-daily-usage* | head -1`
-	echo "Report file 2: ${reportFilename2}"
+	echo "Report file 2: ${reportFilename4}"
 	cp ${reportFilename4} ./
 
 	ls -al
@@ -33,14 +33,14 @@ if [ "$#" -eq 0 ]; then
 
 	yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
 
-	# MAIL_RECIPIENTS="turbo@unicommerce.com,anurag.mittal@unicommerce.com,accounts@unicommerce.com,financeteam@unicommerce.com,kapil@unicommerce.com,pramod@unicommerce.com"
+	MAIL_RECIPIENTS="turbo@unicommerce.com,anurag.mittal@unicommerce.com,accounts@unicommerce.com,financeteam@unicommerce.com,kapil@unicommerce.com,pramod@unicommerce.com"
 
-	MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
+	# MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com"
 
 	MAIL_SUBJECT="Unifill Sales Report MTD and Daily Usage | ${yesterday_date}"
 	MAIL_CONTENT="Please find the attachment. Report prepared by alpha team."
 
-	mutt -e "set content_type=text/html" -s "${MAIL_SUBJECT}" -a "${reportFilename}" -a "${reportFilename2}" -- "${MAIL_RECIPIENTS}" < mail-content.html
+	mutt -e "set content_type=text/html" -s "${MAIL_SUBJECT}" -a "${reportFilename}" -a "${reportFilename2}" -a "${reportFilename3}" -a "${reportFilename4}" -- "${MAIL_RECIPIENTS}" < mail-content.html
 
 elif [ "$#" -eq 3 ]; then
     echo "Manual parameterized run with arguments: $#"
